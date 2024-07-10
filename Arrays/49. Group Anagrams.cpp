981ed04ce586cd40bb3d8168973c9f1c0607c659
ManasMittal2003 <140882469+ManasMittal2003@users.noreply.github.com>
2024-07-10 15:52:31 +0530

    Update 49. Group Anagrams.cpp

--- Arrays/49. Group Anagrams.cpp
+++ Arrays/49. Group Anagrams.cpp
@@ -14,3 +14,31 @@ public:
         return ans;
     }
 };
+//Solution 2 :
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> mp;
+        for (int i = 0; i < strs.size(); i++) {
+            vector<int> store(26, 0);
+            string curr = strs[i];
+            for (int j = 0; j < curr.length(); j++) {
+                store[curr[j] - 'a']++;
+            }
+            string temp = "";
+            for (int k = 0; k < 26; k++) {
+                while (store[k] != 0) {
+                    temp.push_back('a' + k);
+                    store[k]--;
+                }
+            }
+            mp[temp].push_back(curr);
+        }
+        for (auto i : mp) {
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
