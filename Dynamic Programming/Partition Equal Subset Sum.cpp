589d7f4f60b18304750b754c1313b0fb7efe1a5a
ManasMittal2003 <140882469+ManasMittal2003@users.noreply.github.com>
2024-07-13 00:25:36 +0530

    Update Partition Equal Subset Sum.cpp

--- Dynamic Programming/Partition Equal Subset Sum.cpp
+++ Dynamic Programming/Partition Equal Subset Sum.cpp
@@ -1,26 +1,30 @@
-bool solve(int ind,int curr,int total,int &n,vector<int> &arr,vector<vector<int>> &dp){
-	if(curr==total){
-		return true;
-	}
-	if(ind>=n){
-		return false;
-	}
-	if(curr>total){
-		return false;
-	}
-	if(dp[ind][curr]!=-1){
-		return dp[ind][curr];
-	}
-	bool take=solve(ind+1,curr+arr[ind],total-arr[ind],n,arr,dp);
-	bool ntake=solve(ind+1,curr,total,n,arr,dp);
-	return dp[ind][curr]=(take||ntake);
-}
-bool canPartition(vector<int> &arr, int n)
-{
-	int total=0;
-	for(int i=0;i<n;i++){
-		total+=arr[i];
-	}
-	vector<vector<int>> dp(n,vector<int> (total+1,-1));
-	return solve(0,0,total,n,arr,dp);
-}
+class Solution {
+public:
+    bool solve(int ind, int curr, int& total, int& n, vector<int>& nums,
+               vector<vector<int>>& dp) {
+        if ((curr == total / 2) && (curr * 2 == total)) {
+            return true;
+        }
+        if (curr > total / 2) {
+            return false;
+        }
+        if (ind >= n) {
+            return false;
+        }
+        if (dp[ind][curr] != -1) {
+            return dp[ind][curr];
+        }
+        int take = solve(ind + 1, curr + nums[ind], total, n, nums, dp);
+        int ntake = solve(ind + 1, curr, total, n, nums, dp);
+        return dp[ind][curr] = take || ntake;
+    }
+    bool canPartition(vector<int>& nums) {
+        int total = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            total += nums[i];
+        }
+        vector<vector<int>> dp(n, vector<int>(total + 1, -1));
+        return solve(0, 0, total, n, nums, dp);
+    }
+};
